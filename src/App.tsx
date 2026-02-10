a3007362fd3fbdab8cd30443bbbb1fbcdbd52662
Felipe Augusto <augusto.felipedev@gmail.com>
2026-02-10 08:44:34 -0300

    chore: init vite react ts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import styles from "./App.module.css";
+
+export function App() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 className={styles.title}>Listify</h1>
+      <p>Setup inicial do projeto.</p>
+    </main>
+  );
+}
